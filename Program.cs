c7c0cc5d93cb41b7a49f6db06551e8e02b129212
sufferring <lm.yangcheng@gmail.com>
2023-01-23 17:17:27 +0800

    Add integer multiply and division method

--- Program.cs
+++ Program.cs
@@ -14,4 +14,14 @@ namespace ProGitForProgrammers
     {
         return left - right;
     }
+
+    public int Multiply(int left, int right)
+    {
+        return left * right;
+    }
+
+    public int Divide(int left, int right)
+    {
+        return left / right;
+    }
 }
