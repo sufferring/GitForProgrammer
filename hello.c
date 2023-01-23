493948649fe4cb5ac5537e97fd5e8708862f81c7
sufferring <lm.yangcheng@gmail.com>
2023-01-23 10:15:53 +0800

    forth commit add hello

--- hello.c
+++ hello.c
@@ -6,7 +6,7 @@ int main(void)
     int a[10] = {0}, i;
     printf("lionel messi!\n");
 
-    for (i = 0, i < 10; i++)
+    for (i = 0; i < 10; i++)
     {
         printf("a[%d] = %d\n", i, a[i]);
     }
