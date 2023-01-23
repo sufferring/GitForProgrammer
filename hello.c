ec4d34e2429834dabd3675d7bf67cddacb686340
sufferring <lm.yangcheng@gmail.com>
2023-01-23 10:12:30 +0800

    Add writeline indicating we are in command line

--- hello.c
+++ hello.c
@@ -3,6 +3,12 @@
 
 int main(void)
 {
+    int a[10] = {0}, i;
     printf("lionel messi!\n");
+
+    for (i = 0, i < 10; i++)
+    {
+        printf("a[%d] = %d\n", i, a[i]);
+    }
     return 0;
 }
